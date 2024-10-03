46b50162de4190794366d66dfe4a12d521f2a7d7
Aleksey Gilenko <alesha@gilenko.net>
2024-10-03 11:52:36 +0300

    Added Recursive Binary Search

---
+++ Lab1/src/com/company/B2.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class B2 {
+    static Scanner in;
+    public static void main(String[] args) {
+        in = new Scanner(System.in);
+        int[] bi_arr;
+
+    }
+
+    public static void getArray() {
+        System.out.println("Input number of elements: ");
+        int n = in.nextInt();
+        for (int i = 0; i<n; i++) {
+            bi_arr
+        }
+
+    }
+}
--- Lab1/src/com/company/BinarySearch.java
+++ Lab1/src/com/company/BinarySearch.java
@@ -4,26 +4,45 @@ import java.util.Arrays;
 
 public class BinarySearch {
     public static void main(String[] args) {
-
+        int[] mas = {1, 2,-1, 4, 6, 12, 160, 52};
+        System.out.println(binarySearch(mas, 4));
+        System.out.println(binarySearchRecursion(mas, 4, 0, mas.length));
     }
 
-    public int binarySearch(int[] numList, int target) {
+    public static int binarySearch(int[] numList, int target) {
         Arrays.sort(numList);
-        boolean found = false;
+        int upper = numList.length;
+        int lower = 0;
         int pivot = (numList.length+1)/2;
-        int upper = 
         while (numList[pivot] != target) {
             if (numList[pivot] > target) {
-                pivot = (pivot+1)/2;
+                upper = pivot;
             }
             else {
-                pivot = (pivot + numList.length +1)/2;
+                lower = pivot;
             }
-
+            if (Math.abs(lower-upper)<2 && lower != target && upper != target) {
+                return -1;
+            }
+            pivot = (upper+lower)/2;
         }
+        return pivot;
     }
 
-    public int[] bubble(int[] list, boolean ascending) {
-
+    public static int binarySearchRecursion(int[] numList, int target, int start, int end) {
+        Arrays.sort(numList);
+        int pivot = (start+end)/2;
+        if (numList[pivot] > target) {
+            return binarySearchRecursion(numList, target, start, pivot);
+        }
+        else if (numList[pivot] < target) {
+            return binarySearchRecursion(numList, target, pivot, end);
+        }
+        else if (numList[pivot] == target) return pivot;
+        return -1;
     }
+
+//    public int[] bubble(int[] list, boolean ascending) {
+//
+//    }
 }
