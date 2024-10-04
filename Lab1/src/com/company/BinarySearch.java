be77bc561a3f4b6a626b4a77dd8cd0b1b5ab2f9b
Mc-StArT <alesha@gilenko.net>
2024-10-04 09:46:52 +0300

    Fixed Binary Search

--- Lab1/src/com/company/BinarySearch.java
+++ Lab1/src/com/company/BinarySearch.java
@@ -1,12 +1,17 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BinarySearch {
+    static Scanner in;
     public static void main(String[] args) {
-        int[] mas = {1, 2,-1, 4, 6, 12, 160, 52};
-        System.out.println(binarySearch(mas, 4));
-        System.out.println(binarySearchRecursion(mas, 4, 0, mas.length));
+        in = new Scanner(System.in);
+        int[] mas = getArray();
+        int n = in.nextInt();
+        Arrays.sort(mas);
+        System.out.println(binarySearch(mas, n));
+        System.out.println(binarySearchRecursion(mas, n, 0, mas.length));
     }
 
     public static int binarySearch(int[] numList, int target) {
@@ -30,16 +35,27 @@ public class BinarySearch {
     }
 
     public static int binarySearchRecursion(int[] numList, int target, int start, int end) {
-        Arrays.sort(numList);
+
         int pivot = (start+end)/2;
+        if (numList[pivot] == target) return pivot;
+        if (start > end) return -1;
+
         if (numList[pivot] > target) {
-            return binarySearchRecursion(numList, target, start, pivot);
+            return binarySearchRecursion(numList, target, start, pivot-1);
         }
-        else if (numList[pivot] < target) {
-            return binarySearchRecursion(numList, target, pivot, end);
+        else {
+            return binarySearchRecursion(numList, target, pivot+1, end);
         }
-        else if (numList[pivot] == target) return pivot;
-        return -1;
+    }
+    public static int[] getArray() {
+        System.out.print("Input number of elements: ");
+        int n = in.nextInt();
+        System.out.print("Input elements: ");
+        int[] arr = new int[n];
+        for (int i = 0; i<n; i++) {
+            arr[i] = in.nextInt();
+        }
+        return arr;
     }
 
 //    public int[] bubble(int[] list, boolean ascending) {
